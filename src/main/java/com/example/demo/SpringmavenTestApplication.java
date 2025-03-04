0ceb5e69edcf7ccf285135db3006355833e24c0f
[Mactro4760] <[seho4760@gmail.com]>
2025-03-04 16:00:15 +0900

    first commit

---
+++ src/main/java/com/example/demo/SpringmavenTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmavenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmavenTestApplication.class, args);
+	}
+
+}
